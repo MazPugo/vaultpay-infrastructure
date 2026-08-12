9f2ac55ba989a3dd7a125830b1b1f2753077fb9f
MarzenaPugo <maz.pugo@gmail.com>
2026-08-12 22:15:14 +0100

    Add public subnets

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "marzena-terraform-state"
-    key          = "vaultpay-infrastructure/terraform-learning/terraform.tfstate"
+    key          = "vaultpay/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
     use_lockfile = true
--- main.tf
+++ main.tf
@@ -8,7 +8,27 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "Vaultpay-vpc"
+    Name    = "Vaultpay-vpc"
     Project = "Vaultpay"
+  }
+}
+resource "aws_subnet" "public_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[0]
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "vaultpay-public-subnet-a"
+    Project = "VaultPay"
+  }
+}
+resource "aws_subnet" "public_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[1]
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "vaultpay-public-subnet-b"
+    Project = "VaultPay"
   }
 }
